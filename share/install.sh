3e81736661b3ec1ba62cca301e84bfe3cb249be2
Anufriev Georgy <g@anufriev.info>
2024-11-07 10:59:44 +0500

    change share/install.sh

--- share/install.sh
+++ share/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f ~/.local/share/konsole/my.profile ]; then
+if [[ -f ~/.local/share/konsole/my.profile && -f ~/.config/konsolerc  ]]; then
     sed -i "s/^DefaultProfile=.*/DefaultProfile=my.profile/" ~/.config/konsolerc
 fi
